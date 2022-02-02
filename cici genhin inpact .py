6b95eb56b8e687e57c465698423445acc1776afb
amogus7337 <84619912+amogus7337@users.noreply.github.com>
2022-02-02 20:08:01 +0700

    Add files via upload

---
+++ cici genhin inpact .py
@@ -0,0 +1,13 @@
+n =  (input())
+a =  (input())
+c =  (input())
+if a>n:
+    if a>c:
+        print(str(a)+"Больше")
+    else:
+        print(str(c)+"Больше")
+else:
+    if n>c:
+        print(str(n)+"Больше")
+    else:
+        print(str(c)+"Больше")
