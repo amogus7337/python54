701ee84d658a0e7d82b2b0dbbbfe77d9f731e5bd
amogus7337 <84619912+amogus7337@users.noreply.github.com>
2022-02-16 19:54:40 +0700

    Add files via upload

---
+++ -=-дед инсайд-=-.py
@@ -0,0 +1,7 @@
+magazin = ["машинка" , "кукла" , "телефон", "ремень", "игра", "ручки", "книги"]
+print(magazin)
+a = input ("введите элемент")
+magazin.append(a)
+n = input ("введите элемент")
+magazin.append(n)
+print(magazin)
